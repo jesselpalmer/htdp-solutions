3a51e4885edcb2be09deee435e3a222c3a46a49d
Jesse Palmer <jesselpalmer@gmail.com>
2019-09-10 21:07:02 -0700

    feat: add exercise

---
+++ ch1/exercise1.8.rkt
@@ -0,0 +1,8 @@
+(require 2htdp/image)
+(define cat 🐈)
+(define cat-width (image-width cat))
+(define cat-height (image-height cat))
+(define image-tall-or-wide? 
+  (if (= cat-width cat-height) "square" 
+  (if (> cat-width cat-height) "wide" "tall")))
+image-tall-or-wide?
