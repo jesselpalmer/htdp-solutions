594799d28631f44e3020ae850e61b69b3c7c6f2b
Jesse Palmer <jesselpalmer@gmail.com>
2019-09-14 11:57:36 -0700

    refactor: add blank lines

--- ch1/exercise5.rkt
+++ ch1/exercise5.rkt
@@ -1,5 +1,7 @@
 (require 2htdp/image)
+
 (define (tree h) (triangle h "solid" "green"))
 (define (trunk h) (rectangle (/ h 5) (/ h 3) "solid" "brown"))
 (define (draw-tree h) (above (tree h) (trunk h)))
+
 (draw-tree 70)
