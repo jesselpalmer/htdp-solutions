9d73068106982c1aa15d06f563e24779f414f414
Jesse Palmer <jesselpalmer@gmail.com>
2019-09-21 18:08:20 -0700

    feat: add exercise30

---
+++ ch2/exercise30.rkt
@@ -0,0 +1,17 @@
+(define AVERAGE-CHANGE-PER-TICKET-PRICE-CHANGE 15)
+(define BASE-NUM-OF-ATTENDEES 120)
+(define BASE-TICKET-PRICE-PER-ATTENDEE 5.0)
+(define COST-PER-TICKET-PRICE-CHANGE 0.1)
+(define FIXED-COST-PER-ATTENDEE 0.04)
+(define PRICE-SENSITIVITY-FACTOR (/ AVERAGE-CHANGE-PER-TICKET-PRICE-CHANGE COST-PER-TICKET-PRICE-CHANGE))
+
+(define (attendees ticket-price)
+  (- BASE-NUM-OF-ATTENDEES
+     (* (- ticket-price BASE-TICKET-PRICE-PER-ATTENDEE PRICE-SENSITIVITY-FACTOR)))
+(define (revenue ticket-price)
+  (* ticket-price (attendees ticket-price)))
+(define (cost ticket-price)
+  (* FIXED-COST-PER-ATTENDEE (attendees ticket-price)))
+(define (profit ticket-price)
+  (- (revenue ticket-price)
+     (cost ticket-price)))
