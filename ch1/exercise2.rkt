74449677ab3968300e3ce7bd0317fb4752db538d
Jesse Palmer <jesselpalmer@gmail.com>
2019-09-21 17:46:16 -0700

    refactor: add comment

--- ch1/exercise2.rkt
+++ ch1/exercise2.rkt
@@ -1,4 +1,4 @@
-(define prefix "hello")
+(define prefix "hello") ; prefix and suffix should probably be uppercase, but the book has them as lowercase
 (define suffix "world")
 
 (string-append prefix "_" suffix)
