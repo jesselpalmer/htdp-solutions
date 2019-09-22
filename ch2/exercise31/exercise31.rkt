8372af8ee2b79aaceb55ad612bb0e14bc8539173
Jesse Palmer <jesselpalmer@gmail.com>
2019-09-21 22:58:44 -0700

    feat: add exercise31

---
+++ ch2/exercise31/exercise31.rkt
@@ -0,0 +1,33 @@
+(require 2htdp/batch-io)
+
+(define (letter fst lst signature-name)
+  (string-append
+    (opening fst)
+    "\n\n"
+    (body fst lst)
+    "\n\n"
+    (closing signature-name)))
+ 
+(define (opening fst)
+  (string-append "Dear " fst ","))
+ 
+(define (body fst lst)
+  (string-append
+   "We have discovered that all people with the" "\n"
+   "last name " lst " have won our lottery. So, " "\n"
+   fst ", " "hurry and pick up your prize."))
+ 
+(define (closing signature-name)
+  (string-append
+   "Sincerely,"
+   "\n\n"
+   signature-name
+   "\n"))
+
+(define (main in-fst in-lst in-signature out)
+  (write-file out
+              (letter (read-file in-fst)
+                      (read-file in-lst)
+                      (read-file in-signature))))
+
+(main "fst.dat" "lst.dat" "signature.dat" 'stdout)
